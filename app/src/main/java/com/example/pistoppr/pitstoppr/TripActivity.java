f88367ee298e367289424d16ada275ca9822d8ef
suzmue <suzmue@mit.edu>
2015-04-01 16:59:59 -0400

    changes

--- app/src/main/java/com/example/pistoppr/pitstoppr/TripActivity.java
+++ app/src/main/java/com/example/pistoppr/pitstoppr/TripActivity.java
@@ -4,25 +4,21 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.content.Intent;
 
 
-
-public class MainActivity extends ActionBarActivity {
-    public String test;
+public class TripActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_trip);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_trip, menu);
         return true;
     }
 
@@ -39,19 +35,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /** Called when the user clicks the Start Trip Button */
-    public void startTrip(View view){
-        //Start a new view with the trip
-        Intent intent = new Intent(this, TripActivity.class);
-        startActivity(intent);
-    }
-
-    /** Called when the user clicks the Settings Button */
-    public void openSettings(View view){
-        //Opens the settings
-        Intent intent = new Intent(this, Settings2Activity.class);
-        startActivity(intent);
     }
 }
