34ef03b5e49259c215a991546cc9649fa222f1bc
Katie <kpvm@mit.edu>
2015-04-01 19:53:27 -0400

    started to add dropdown menu for mile range in settings activity

--- app/src/main/java/com/example/pistoppr/pitstoppr/Settings2Activity.java
+++ app/src/main/java/com/example/pistoppr/pitstoppr/Settings2Activity.java
@@ -36,4 +36,13 @@ public class Settings2Activity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    android.widget.Spinner spinner = (android.widget.Spinner) findViewById(R.id.spinner);
+    // Create an ArrayAdapter using the string array and a default spinner layout
+    android.widget.ArrayAdapter<CharSequence> adapter = android.widget.ArrayAdapter.createFromResource(this,
+            R.array.miles_range_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+    spinner.setAdapter(adapter);
 }
